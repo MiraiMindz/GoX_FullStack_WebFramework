b1696880d6081cf8450a5fb55d8800e5e9f61088
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2024-03-21 00:54:21 -0300

    automatic edit commit

--- FRAMEWORK/wasm/main_wasm.go
+++ FRAMEWORK/wasm/main_wasm.go
@@ -1,26 +1,15 @@
 //go:build wasm && !amd64
 // +build wasm,!amd64
 
-package wasm
+package main
 
 import (
 	"syscall/js"
-
-	"FRAMEWORK/internal/html"
 )
 
 func main() {
 	app := js.Global().Get("document").Call("getElementById", "_INTERNAL_GOX_APP")
-
-	s := html.CreateBareHTMLTemplate("App", `
-<main>
-	<h1>Hello from: {{.First}} and {{.Second}}</h1>
-</main>
-		`, struct {
-		First  string
-		Second string
-	}{First: "Hello", Second: "World"})
-
+	s := "Teste"
 	app.Set("innerHTML", s)
-
+	select {}
 }
